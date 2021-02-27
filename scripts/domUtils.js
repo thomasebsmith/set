37b54ac060360e83e3789b275b6654475cc5e2b7
Thomas Smith <thsm@umich.edu>
2021-02-26 23:53:00 -0500

    Add comments to domUtils script

--- scripts/domUtils.js
+++ scripts/domUtils.js
@@ -1,4 +1,6 @@
 (function(global) {
+  // Convert a query string of the form "?key=value&..."
+  // into an object of the form {"key": "value", ...}.
   global.parseQuery = (query = global.location.search) => {
     return Object.fromEntries(query.substring(1).split("&").map(keyVal =>
       keyVal.split("=").map(global.decodeURIComponent)
