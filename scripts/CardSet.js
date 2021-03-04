ec5599efdce54617244bf58b887788f0fbc939bd
Thomas Smith <42979377+thomasebsmith@users.noreply.github.com>
2021-03-03 23:48:18 -0500

    Add comments to CardSet class

--- scripts/CardSet.js
+++ scripts/CardSet.js
@@ -1,7 +1,13 @@
+// Represents a set of three cards. The set may or may not form
+// a valid Set that matches or differs in all characteristics.
 class CardSet {
+  // Used for serialization and deserialization.
   static _jsonVersion = 0;
+
+  // The number of cards needed.
   static size = 3;
 
+  // cards should be an array of Cards.
   constructor(cards) {
     this._cards = cards;
     if (this._cards.length !== CardSet.size) {
@@ -14,12 +20,16 @@ class CardSet {
     return attrSet.size === this._cards.length || attrSet.size === 1;
   }
 
+  // Returns whether all cards in this set match or differ in all
+  // characteristics.
   isValid() {
     return ["number", "shape", "color", "pattern"].every(
       (attr) => this._isValidAttr(attr)
     );
   }
 
+  // Serializes this CardSet to a JSON-compatible
+  // object.
   toJSON() {
     return {
       cards: this._cards.map(card => card.toJSON()),
@@ -27,6 +37,8 @@ class CardSet {
     };
   }
 
+  // Deserializes a CardSet from a JSON-compatible
+  // object.
   static fromJSON(json) {
     if (json.jsonVersion !== 0) {
       throw Error("Invalid JSON version");
