8fefc29cb0fb4d01d3404f79dbdf88c684d7cb14
Thomas Smith <42979377+thomasebsmith@users.noreply.github.com>
2021-03-25 23:51:48 -0400

    Add comments to shared.js

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,8 +1,11 @@
 window.shared = (function(global) {
+  // Returns the index of a uniformly random element of
+  //  `array`.
   function randomIndex(array) {
     return Math.floor(Math.random() * array.length);
   }
 
+  // Returns a uniformly random element from `array`.
   function randomElement(array) {
     return array[randomIndex(array)];
   }
